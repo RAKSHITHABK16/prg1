796f828684938ebb9e24264dfff84d47a06d1ba6
Rakshithabk <rakshithabk16@gmail.com>
2020-07-18 14:50:13 +0530

    template added

--- prg1/views.py
+++ prg1/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("hello world")
 
 def home(request):
     return HttpResponse("<h1>Welcome to Homepage</h1>")
+
+def html_demo1(request):
+    return render(request,"sample1.html")
+
+def html_demo2(request):
+    return render(request,"sample2.html")
